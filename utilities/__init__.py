dba03360a443cc79ded3bd9aa6d2a595cd25a592
ndonkoHenri <robotcoder4@protonmail.com>
2023-06-21 01:27:54 +0200

    update __init__.py

--- utilities/__init__.py
+++ utilities/__init__.py
@@ -1,6 +1,6 @@
 try:
     from .section import Section, SubSection
-    from .section_components import actions_section
+    from .section_components import actions_section, selection_section, containment_section, communication_section, navigation_section, text_inputs_section
 except ModuleNotFoundError:
     from section import Section, SubSection
-    from section_components import actions_section
+    from section_components import actions_section, selection_section, containment_section, communication_section, navigation_section, text_inputs_section
