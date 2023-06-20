c52783ffb062a67ba9653f0bc2016de779196335
ndonkoHenri <robotcoder4@protonmail.com>
2023-06-21 01:26:39 +0200

    more sections

--- utilities/section_components.py
+++ utilities/section_components.py
@@ -117,6 +117,136 @@ dialogs = ft.Row(
 
 dividers = ft.Divider(height=5, thickness=1)
 
+# Navigation Components
+# TBD
+
+# Selection Components
+checkboxes = ft.ListView(
+    controls=[
+        ft.ListTile(title=ft.Text("Option 1"), trailing=ft.Checkbox(value=True, tristate=True), toggle_inputs=True),
+        ft.ListTile(title=ft.Text("Option 2"), trailing=ft.Checkbox(value=True, tristate=True), toggle_inputs=True),
+        ft.ListTile(title=ft.Text("Option 3"), trailing=ft.Checkbox(tristate=True), toggle_inputs=True),
+        ft.ListTile(title=ft.Text("Option 4"), trailing=ft.Checkbox(value=False), disabled=True)
+    ]
+)
+
+date_picker = ft.TextButton("Show date picker")
+
+
+def dropdown_change(e):
+    i = menus.controls[1].controls[2]
+    if e.control.data == "icon":
+        i.icon = e.control.value.lower()
+    elif e.control.data == "color":
+        i.color = e.control.value.lower()
+    menus.update()
+
+
+menus = ft.Column(
+    controls=[
+        ft.Row(
+            controls=[
+                ft.PopupMenuButton(
+                    ft.FilledTonalButton("Show menu"),
+                    items=[
+                        ft.PopupMenuItem(icon=ft.icons.PERSON_2, content=ft.Text("Item 1")),
+                        ft.PopupMenuItem(icon=ft.icons.PANORAMA_FISHEYE, content=ft.Text("Item 2")),
+                        ft.PopupMenuItem(icon=ft.icons.RESTART_ALT, content=ft.Text("Item 3"))
+                    ]
+                ),
+                ft.PopupMenuButton(
+                    ft.Icon(ft.icons.MENU),
+                    items=[
+                        ft.PopupMenuItem(content=ft.Text("Menu 1")),
+                        ft.PopupMenuItem(content=ft.Text("Menu 2")),
+                        ft.PopupMenuItem(content=ft.Text("Menu 3"), )
+                    ]
+                )
+            ]
+        ),
+        ft.Row(
+            controls=[
+                ft.Dropdown(
+                    label="Color",
+                    options=[
+                        ft.dropdown.Option("Blue"),
+                        ft.dropdown.Option("Pink"),
+                        ft.dropdown.Option("Green"),
+                        ft.dropdown.Option("Yellow"),
+                        ft.dropdown.Option("Grey", disabled=True),
+                    ],
+                    on_change=dropdown_change,
+                    data="color",
+                    width=100
+                ),
+                ft.Dropdown(
+                    label="Icon",
+                    prefix_icon=ft.icons.SEARCH,
+                    options=[
+                        # ft.dropdown.Option("Smile"),
+                        ft.dropdown.Option("Cloud"),
+                        ft.dropdown.Option("Brush"),
+                        ft.dropdown.Option("Heart"),
+                    ],
+                    value="Cloud",
+                    on_change=dropdown_change,
+                    data="icon"
+                ),
+                ft.Icon(ft.icons.CLOUD)
+            ]
+        )
+    ]
+)
+
+radio_buttons = ft.RadioGroup(
+    content=ft.ListView(
+        controls=[
+            ft.ListTile(title=ft.Text("Option 1"), leading=ft.Radio(value="1"), toggle_inputs=True),
+            ft.ListTile(title=ft.Text("Option 2"), leading=ft.Radio(value="2"), toggle_inputs=True),
+            ft.ListTile(title=ft.Text("Option 3"), leading=ft.Radio(value="3"), disabled=True),
+        ]
+    ),
+    value="1"
+)
+
+sliders = ft.Column(
+    controls=[
+        ft.Slider(min=0, max=100),
+        ft.Slider(min=0, max=100, divisions=5)
+    ]
+)
+
+switches = ft.Row(
+    controls=[
+        ft.Column(
+            controls=[
+                ft.Switch(value=False),
+                ft.Switch(value=False, disabled=True)
+            ]
+        ),
+        ft.Column(
+            controls=[
+                ft.Switch(value=True),
+                ft.Switch(value=True, disabled=True)
+            ]
+        ),
+    ],
+    alignment=ft.MainAxisAlignment.SPACE_BETWEEN
+)
+
+time_picker = ft.TextButton("Show time picker")
+
+# Text Input Components
+text_inputs = ft.Column(
+    controls=[
+        ft.TextField(
+            helper_text="supporting text"
+        )
+    ]
+)
+
+# Classifying Sections and SubSections
+
 actions_section = Section(
     "Actions",
     [
