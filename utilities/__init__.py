e3961887117385be6c6857cbb947f1367fd9575a
ndonkoHenri <robotcoder4@protonmail.com>
2023-07-06 01:48:44 +0200

    add TabContent

--- utilities/__init__.py
+++ utilities/__init__.py
@@ -1,8 +1,8 @@
 try:
-    from .components_utils import ComponentSection, ComponentSubSection, BottomSheetButton, CardContainer, InputFields
+    from .components_utils import ComponentSection, ComponentSubSection, BottomSheetButton, CardContainer, InputFields, TabContent
     from .color_utils import ColorContainer
     from .elevation_utils import ElevationSection
 except ModuleNotFoundError:
-    from components_utils import ComponentSection, ComponentSubSection, BottomSheetButton, CardContainer, InputFields
+    from components_utils import ComponentSection, ComponentSubSection, BottomSheetButton, CardContainer, InputFields, TabContent
     from color_utils import ColorContainer
     from elevation_utils import ElevationSection
