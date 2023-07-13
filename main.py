5ca15a5649bb1d026e7219903ed0d43cf6a72688
ndonkoHenri <robotcoder4@protonmail.com>
2023-07-13 22:19:40 +0200

    update main.py

--- main.py
+++ main.py
@@ -2,72 +2,143 @@ import flet as ft
 from screens import TypographyScreen, ComponentScreen, ColorScreen, ElevationScreen
 
 
-class Main(object):
+class Main:
     def __init__(self):
         self.page: ft.Page | None = None
+        self.screens = [ComponentScreen(), ColorScreen(), TypographyScreen(), ElevationScreen()]
+        self.width_breakpoint = 994
+        self.nav_rail = ft.NavigationRail(
+            selected_index=0,
+            label_type=ft.NavigationRailLabelType.NONE,
+            group_alignment=-1.0,
+            destinations=[
+                ft.NavigationRailDestination(
+                    icon=ft.icons.SETTINGS_INPUT_COMPONENT_OUTLINED,
+                    selected_icon=ft.icons.SETTINGS_INPUT_COMPONENT,
+                    label="Components"
+                ),
+                ft.NavigationRailDestination(
+                    icon=ft.icons.FORMAT_PAINT_OUTLINED,
+                    selected_icon=ft.icons.FORMAT_PAINT,
+                    label="Colors"
+                ),
+                ft.NavigationRailDestination(
+                    icon=ft.icons.TYPE_SPECIMEN_OUTLINED,
+                    selected_icon=ft.icons.TYPE_SPECIMEN,
+                    label="Typography"
+                ),
+                ft.NavigationRailDestination(
+                    icon=ft.icons.FORMAT_PAINT_OUTLINED,
+                    selected_icon=ft.icons.FORMAT_PAINT,
+                    label="Elevation"
+                )
+            ],
+            on_change=self.handle_nav_rail_change,
+            trailing=ft.Column(self._action_buttons()),
+            # visible=True if self.page.width >= self.width_breakpoint else False,
+            expand=1,
+            height=550
+        )
+
+    def _action_buttons(self):
+        actions = [
+            ft.IconButton(
+                ft.icons.DARK_MODE_OUTLINED,
+                selected_icon=ft.icons.WB_SUNNY_OUTLINED,
+                on_click=self.handle_brightness_change,
+                tooltip="Toggle brightness"
+            ),
+            ft.IconButton(
+                ft.icons.FILTER_2,
+                selected_icon=ft.icons.FILTER_3,
+                on_click=self.handle_material_version_change,
+                tooltip="Switch to Material 2",
+            ),
+            ft.PopupMenuButton(
+                icon=ft.icons.COLOR_LENS_OUTLINED,
+                items=[
+                    ft.PopupMenuItem(
+                        content=ft.Row(
+                            controls=[
+                                ft.Icon(ft.icons.COLOR_LENS_OUTLINED, color=clr),
+                                ft.Text(clr.title())
+                            ]
+                        ),
+                        data=clr,
+                        on_click=self.handle_seed_color_change
+                    ) for clr in [
+                        # "M3 Baseline",
+                        "indigo",
+                        "blue",
+                        "teal",
+                        "green",
+                        "yellow",
+                        "orange",
+                        "deeporange",
+                        "pink"
+                    ]
+                ],
+                tooltip="Select a seed color"
+            ),
+            ft.IconButton(
+                ft.icons.IMAGE_OUTLINED
+            )
+        ]
+
+        return actions
 
     def run(self, page):
         self.page: ft.Page = page
         self.page.title = "Flet Material 3 Demo"
-        self.page.window_height, self.page.window_width = 550, 500
-        # self.page.on_resize = lambda e: print(self.page.window_width)
-        self.page.scroll = ft.ScrollMode.AUTO
+        self.page.window_height, self.page.window_width = 550, 1000
+        self.page.on_resize = self.handle_page_resize
+        self.page.scroll = ft.ScrollMode.HIDDEN
+        self.page.padding = ft.Padding(5, 0, 5, 0)
         # self.page.window_always_on_top = True
         self.page.theme_mode = "light"
         self.page.theme = self.page.dark_theme = ft.Theme(use_material3=True)
+        self.nav_rail.visible = True if self.page.width >= self.width_breakpoint else False
+
         self.page.appbar = ft.AppBar(
             title=ft.Text("Material 3"),
-            actions=[
-                ft.IconButton(
-                    ft.icons.DARK_MODE_OUTLINED,
-                    selected_icon=ft.icons.WB_SUNNY_OUTLINED,
-                    on_click=self.handle_brightness_change,
-                    tooltip="Toggle brightness"
+            actions=None if self.page.width >= self.width_breakpoint else self._action_buttons()
+        )
+        self.page.navigation_bar = ft.NavigationBar(
+            destinations=[
+                ft.NavigationDestination(
+                    icon=ft.icons.SETTINGS_INPUT_COMPONENT_OUTLINED,
+                    selected_icon=ft.icons.SETTINGS_INPUT_COMPONENT,
+                    label="Components"
                 ),
-                ft.IconButton(
-                    ft.icons.FILTER_2,
-                    selected_icon=ft.icons.FILTER_3,
-                    on_click=self.handle_material_version_change,
-                    tooltip="Switch to Material 2"
+                ft.NavigationDestination(
+                    icon=ft.icons.FORMAT_PAINT_OUTLINED,
+                    selected_icon=ft.icons.FORMAT_PAINT,
+                    label="Colors"
                 ),
-                ft.PopupMenuButton(
-                    content=ft.Icon(
-                        ft.icons.COLOR_LENS_OUTLINED,
-                    ),
-                    items=[
-                        ft.PopupMenuItem(
-                            content=ft.Row(
-                                controls=[
-                                    ft.Icon(ft.icons.COLOR_LENS_OUTLINED, color=clr),
-                                    ft.Text(clr.title())
-                                ]
-                            ),
-                            data=clr,
-                            on_click=self.handle_seed_color_change
-                        ) for clr in [
-                            # "M3 Baseline", 
-                            "indigo",
-                            "blue",
-                            "teal",
-                            "green",
-                            "yellow",
-                            "orange",
-                            "deeporange",
-                            "pink"
-                        ]
-                    ],
-                    tooltip="Select a seed color"
+                ft.NavigationDestination(
+                    icon=ft.icons.TYPE_SPECIMEN_OUTLINED,
+                    selected_icon=ft.icons.TYPE_SPECIMEN,
+                    label="Typography"
                 ),
-                ft.IconButton(
-                    ft.icons.IMAGE_OUTLINED
-                )
-            ]
+                ft.NavigationDestination(
+                    icon=ft.icons.FORMAT_PAINT_OUTLINED,
+                    selected_icon=ft.icons.FORMAT_PAINT,
+                    label="Elevation"
+                ),
+            ],
+            on_change=self.handle_nav_bar_change,
+            visible=False if self.page.width >= self.width_breakpoint else True
         )
 
         self.page.add(
-            components,
-            # typography
-            # colors
+            ft.Row(
+                controls=[
+                    self.nav_rail,
+                    self.screens[0]
+                ],
+                alignment=ft.MainAxisAlignment.START,
+                vertical_alignment=ft.CrossAxisAlignment.START
+            )
         )
 
     def handle_brightness_change(self, e):
@@ -75,16 +146,44 @@ class Main(object):
         self.page.theme_mode = "light" if self.page.theme_mode == "dark" else "dark"
         self.page.update()
 
-    def handle_material_version_change(self, e: ft.ControlEvent):
-        # e.control.selected = True                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         nnnnn
-        e.control.tooltip = f"Switch to Material {2 if self.page.theme.use_material3 == True else 3}"
+    def handle_material_version_change(self, e):
+        e.control.selected = not e.control.selected
         self.page.theme = self.page.dark_theme = ft.Theme(use_material3=not self.page.theme.use_material3)
+        e.control.tooltip = f"Switch to Material {2 if self.page.theme.use_material3 == True else 3}"
         self.page.appbar.title.value = f"Material {3 if self.page.theme.use_material3 == True else 2}"
         self.page.update()
 
     def handle_seed_color_change(self, e):
-        e.control.disabled = True
         self.page.theme.color_scheme_seed = self.page.dark_theme.color_scheme_seed = e.control.data
+        # update icon of the selected color
+        for i in self.page.appbar.actions[2].items:
+            # deactivate all
+            if i.content.controls[0].name == ft.icons.COLOR_LENS:
+                i.content.controls[0].name = ft.icons.COLOR_LENS_OUTLINED
+            # set the clicked one
+            if i.content.controls[0].color == e.control.data:
+                i.content.controls[0].name = ft.icons.COLOR_LENS
+        self.page.update()
+
+    def handle_page_resize(self, e):
+        print(self.page.width, self.page.window_width, self.page.height, self.page.window_height)
+        if self.page.width >= self.width_breakpoint:
+            self.page.navigation_bar.visible = False
+            self.nav_rail.visible = True
+            self.page.appbar.actions = None
+        else:
+            self.page.navigation_bar.visible = True
+            self.nav_rail.visible = False
+            self.page.appbar.actions = self._action_buttons()
+
+        self.page.update()
+
+    def handle_nav_bar_change(self, e: ft.ControlEvent):
+        self.page.controls[0].controls[-1] = self.screens[int(e.data)]
+        self.page.update()
+
+    def handle_nav_rail_change(self, e: ft.ControlEvent):
+        self.page.controls[0].controls[-1] = self.screens[int(e.data)]
         self.page.update()
 
 
