2527590d5aa89891219f1d89b5d994898aeb105c
ndonkoHenri <robotcoder4@protonmail.com>
2023-07-13 22:03:29 +0200

    use a Class

--- screens/elevation_screen.py
+++ screens/elevation_screen.py
@@ -1,11 +1,12 @@
 import flet as ft
 from Flet_Material3_Demo.utilities import ElevationSection
 
-elevation = ft.Column(
-    controls=[
-        ElevationSection("Surface Tint Only", surface_tint=ft.colors.PRIMARY, is_top=True),
-        ElevationSection("Surface Tint and Shadow", surface_tint=ft.colors.PRIMARY, shadow_color=ft.colors.SHADOW),
-        ElevationSection("Shadow Only", shadow_color=ft.colors.SHADOW),
-    ],
-    spacing=10
-)
+
+class ElevationScreen(ft.Column):
+    def __init__(self):
+        super().__init__(spacing=10, expand=19)
+        self.controls = [
+            ElevationSection("Surface Tint Only", surface_tint=ft.colors.PRIMARY, is_top=True),
+            ElevationSection("Surface Tint and Shadow", surface_tint=ft.colors.PRIMARY, shadow_color=ft.colors.SHADOW),
+            ElevationSection("Shadow Only", shadow_color=ft.colors.SHADOW),
+        ]
